1c2e9d587eb85f23fa781b19bcd7f9597c1cfc63
xLillium <nicolas.motillon@gmail.com>
2023-09-13 23:35:08 +0200

    Add test for the Lexer readChar method

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,10 +1,15 @@
 package lexer
 
 type Lexer struct {
-	input string
+	input       string
+	currentChar byte
+	currentPos  int
+	nextPos     int
 }
 
 func New(input string) *Lexer {
 	return &Lexer{input: input}
 }
 
+func (l *Lexer) readChar() {
+}
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -12,3 +12,34 @@ func TestLexerInitialization(t *testing.T) {
 		t.Fatalf("Expected lexer input to be %s, got %s", input, lexer.input)
 	}
 }
+
+func TestReadChar(t *testing.T) {
+	input := "abc"
+	lexer := New(input)
+
+	lexer.readChar()
+	if lexer.currentChar != 'a' {
+		t.Fatalf("Expected first char to be 'a', got '%c'", lexer.currentChar)
+	}
+	if lexer.currentPos != 0 {
+		t.Fatalf("Expected position to be 0, got %d", lexer.currentPos)
+	}
+	if lexer.nextPos != 1 {
+		t.Fatalf("Expected readPosition to be 1, got %d", lexer.nextPos)
+	}
+
+	lexer.readChar()
+	if lexer.currentChar != 'b' {
+		t.Fatalf("Expected second char to be 'b', got '%c'", lexer.currentChar)
+	}
+
+	lexer.readChar()
+	if lexer.currentChar != 'c' {
+		t.Fatalf("Expected third char to be 'c', got '%c'", lexer.currentChar)
+	}
+
+	lexer.readChar()
+	if lexer.currentChar != 0 {
+		t.Fatalf("Expected ch to be 0 after reaching end, got '%c'", lexer.currentChar)
+	}
+}
