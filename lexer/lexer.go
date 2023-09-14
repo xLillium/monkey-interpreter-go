96c6de3fdb41a48d09c11e5461643add4ef43e1a
xLillium <nicolas.motillon@gmail.com>
2023-09-14 22:52:33 +0200

    Add implementation for Lexer.NextToken() Monkey source code support

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -34,6 +34,8 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.currentChar {
 	case '=':
 		tok = token.Token{Type: token.ASSIGN, Literal: string(l.currentChar)}
@@ -54,9 +56,49 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok = token.Token{Type: token.EOF, Literal: ""}
 	default:
-		tok = token.Token{Type: token.ILLEGAL, Literal: string(l.currentChar)}
+		if isLetter(l.currentChar) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.currentChar) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = token.Token{Type: token.ILLEGAL, Literal: string(l.currentChar)}
+		}
 	}
 
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.currentChar == ' ' || l.currentChar == '\t' || l.currentChar == '\n' || l.currentChar == '\r' {
+		l.readChar()
+	}
+}
+
+func isLetter(b byte) bool {
+	return 'a' <= b && b <= 'z' || 'A' <= b && b <= 'Z' || b == '_'
+}
+
+func (l *Lexer) readIdentifier() string {
+	startPos := l.currentPos
+	for isLetter(l.currentChar) {
+		l.readChar()
+	}
+	return l.input[startPos:l.currentPos]
+}
+
+func isDigit(b byte) bool {
+	return '0' <= b && b <= '9'
+}
+
+func (l *Lexer) readNumber() string {
+	startPos := l.currentPos
+	for isDigit(l.currentChar) {
+		l.readChar()
+	}
+	return l.input[startPos:l.currentPos]
 }
