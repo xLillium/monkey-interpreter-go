b50701475a4d367fe4bdddd3b89f8c46d134d85d
xLillium <nicolas.motillon@gmail.com>
2023-09-15 00:18:34 +0200

    Fix Lexer TestReadCharProgression to be more accurate

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -37,19 +37,15 @@ func TestReadCharProgression(t *testing.T) {
 	input := "abc"
 	lexer := New(input)
 
-	if lexer.currentChar != 'a' {
-		t.Fatalf("Expected current char to be 'a', got '%c'", lexer.currentChar)
-	}
-	if lexer.currentPos != 0 {
-		t.Fatalf("Expected current position to be 0, got %d", lexer.currentPos)
-	}
-	if lexer.nextPos != 1 {
-		t.Fatalf("Expected next position to be 1, got %d", lexer.nextPos)
-	}
-
 	lexer.readChar()
 	if lexer.currentChar != 'b' {
-		t.Fatalf("Expected current char to be 'b', got '%c'", lexer.currentChar)
+		t.Fatalf("Expected current char to be 'a', got '%c'", lexer.currentChar)
+	}
+	if lexer.currentPos != 1 {
+		t.Fatalf("Expected current position to be 0, got %d", lexer.currentPos)
+	}
+	if lexer.nextPos != 2 {
+		t.Fatalf("Expected next position to be 1, got %d", lexer.nextPos)
 	}
 
 	lexer.readChar()
