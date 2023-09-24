1979fa277413535445fffcf7f05508c382b50cd7
xLillium <nicolas.motillon@gmail.com>
2023-09-24 12:59:45 +0200

    Add If statements to parsing

--- parser/parser.go
+++ parser/parser.go
@@ -74,6 +74,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
+	p.registerPrefix(token.IF, p.parseIfExpression)
 
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
 	p.registerInfix(token.MINUS, p.parseInfixExpression)
@@ -238,6 +239,37 @@ func (p *Parser) parseGroupedExpression() ast.Expression {
 		return nil
 	}
 	return expression
+}
+
+func (p *Parser) parseIfExpression() ast.Expression {
+	expression := &ast.IfExpression{Token: p.current}
+	if !p.advanceIfPeekIs(token.LPAREN) {
+		return nil
+	}
+	p.advanceToken()
+	expression.Condition = p.parseExpression(LOWEST)
+	if !p.advanceIfPeekIs(token.RPAREN) {
+		return nil
+	}
+	if !p.advanceIfPeekIs(token.LBRACE) {
+		return nil
+	}
+	expression.Consequence = p.parseBlockStatement()
+	return expression
+}
+
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.current}
+	block.Statements = []ast.Statement{}
+	p.advanceToken()
+	for !p.tokenIs(p.current, token.RBRACE) && !p.tokenIs(p.current, token.EOF) {
+		statement := p.parseStatement()
+		if statement != nil {
+			block.Statements = append(block.Statements, statement)
+		}
+		p.advanceToken()
+	}
+	return block
 }
 
 // Token navigation and validation functions.
